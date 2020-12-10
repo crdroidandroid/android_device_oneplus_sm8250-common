5154563cf57efe249eb55dfd8284f09bfdca5b0b
firebird11 <hbgassel@gmail.com>
2020-12-10 18:29:54 +0100

    sm8250-common: GoogleCamera
    Change-Id: I39efc5e95f9b37067c4c05e8fb0091e39921b6a8

--- common.mk
+++ common.mk
@@ -26,6 +26,9 @@ $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
 # Gapps
 $(call inherit-product, vendor/google/gms/config.mk)
 
+# GoogleCamera
+$(call inherit-product, packages/apps/GoogleCamera/config.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
