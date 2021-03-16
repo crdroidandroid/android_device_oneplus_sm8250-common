06395dab27cf393609ea78834c57637aea22f6aa
Pig <pig.priv@gmail.com>
2021-03-16 19:57:23 +0300

    sm8250-common: Move DPM feature property to /system_ext
    * DPM blobs are under /system_ext.
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>
    Change-Id: Ia3bef45067265a6cad851ecd182147e0aaf7ef63
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,6 +63,7 @@ TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 
 # A/B
 AB_OTA_UPDATER := true
