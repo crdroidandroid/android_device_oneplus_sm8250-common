e9cddfcf3efc42bebd30c060fd462ed52a15064e
firebird11 <hbgassel@gmail.com>
2021-04-12 17:05:01 +0300

    Revert "sm8250-common: Build Gapps"
    This reverts commit bf7a76e8f39690930090de011f086e45f4e1eb4d.

--- common.mk
+++ common.mk
@@ -27,9 +27,6 @@ PRODUCT_COPY_FILES += \
 # GoogleCamera
 $(call inherit-product, packages/apps/GoogleCamera/config.mk)
 
-# Gapps
-$(call inherit-product, vendor/google/gms/config.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
