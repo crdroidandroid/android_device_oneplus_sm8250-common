491c417a03e945bd4333225e9c389626d424b355
firebird11 <hbgassel@gmail.com>
2021-05-12 18:42:39 +0300

    Revert "sm8250-common: Nuke updatable APEX"
    This reverts commit fddab7fde551455d80ab9dc322e9579c87e16315.

--- common.mk
+++ common.mk
@@ -14,6 +14,9 @@
 # limitations under the License.
 #
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Include GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
