c8d8c8b3b19bcfd5ef4167f21c66ff1d7c6df9f2
firebird11 <hbgassel@gmail.com>
2021-05-21 13:08:43 +0300

    sm8250-common: DeviceSettings
    Co-authored-by: darkobas <darkobas@gmail.com>
    Co-authored-by: Max Weninger <max.weninger@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: kldoc <fred.berbigier@gmail.com>
    Co-authored-by: Christian Oder <myself5@carbonrom.org>
    Co-authored-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-authored-by: David Sn <divad.nnamtdeis@gmail.com>
    Co-authored-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Change-Id: Icc1a38da16841e00a921cecc061c0b79abef956f

--- common.mk
+++ common.mk
@@ -203,6 +203,7 @@ PRODUCT_PACKAGES += \
     init.oem.rc \
     init.oem.sec.rc \
     init.oem_ftm.rc \
+    init.opcamera.rc \
     init.oplus_chg.sh \
     init.qcom.class_core.sh \
     init.qcom.coex.sh \
@@ -228,6 +229,10 @@ PRODUCT_PACKAGES += \
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
 
 # Display
 PRODUCT_PACKAGES += \
@@ -447,7 +452,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Trust HAL
