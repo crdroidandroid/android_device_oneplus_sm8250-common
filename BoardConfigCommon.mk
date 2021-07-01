d4c455b544cfbebc909bd15fd37e1d0091640080
ccfries <cfries@google.com>
2021-07-01 15:35:17 +0800

    sm8250-common: Set VENDOR_SECURITY_PATCH to platform
    * To match the platform security patch level.
      They are functionally the same
    
    Bug: 125914443
    Change-Id: Ib77ab115d6faabd21958c34e2dae49e
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,7 +184,7 @@ CUSTOM_APNS_FILE := $(COMMON_PATH)/configs/apns-conf.xml
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-08-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
