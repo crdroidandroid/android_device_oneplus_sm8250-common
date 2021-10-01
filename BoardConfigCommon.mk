4d06565c07316a4038a24e54409055c76b79560f
AnierinB <anierin@evolution-x.org>
2021-09-30 23:55:01 -0700

    Revert "sm8250-common: Include device FCM from vendor/lineage"
    This reverts commit 7ec6643321a58c18af0b632e6920d0e0628da8f6.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,9 +122,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oneplus:libudfps_extension.oneplus
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/device_framework_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 
