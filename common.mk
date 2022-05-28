8063ddc2ecb0b1311cd1f01da6fdda8226c9d608
SENSEIIIII <mistakesensei@gmail.com>
2022-05-28 12:58:27 +0000

    init crDroid

--- common.mk
+++ common.mk
@@ -39,8 +39,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-derp
-
+    $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
