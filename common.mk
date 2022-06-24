74ee7b1067d895b1900180d3e51d514bc03b736e
Pranav Vashi <neobuddy89@gmail.com>
2022-06-25 02:41:51 +0530

    sm8250-common: Enable blur effect
    * crdroid feature
    * Changes QS transparency into blur effect.
    
    Change-Id: Ib2d7c7b893d0f12e2c52f47ae68f7ab28dda63e2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- common.mk
+++ common.mk
@@ -83,6 +83,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Board API level
 BOARD_SHIPPING_API_LEVEL := 30
 
