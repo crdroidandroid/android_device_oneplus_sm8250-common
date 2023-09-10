3f336d5caf6e0a0d2bb1a06b9240ccb05de8580a
madmax7896 <madmax78968@gmail.com>
2023-09-10 10:16:28 -0300

    sm8250-common: Import Dolby audio blobs and manifest
    Squashed with:
    
    commit c525bc1923414b672d08d01c47a66dba3c8eeeb1
    Author: ShevT <evgeny.shishkov@gmail.com>
    Date:   Wed Nov 27 14:23:02 2024 +0300
    
        sm8150-common: Dolby: Add manifest
    
    commit 0a7aab81667f137c57fa95c4661fa7e43e0e2c1c
    Author: Pranav Vashi <neobuddy89@gmail.com>
    Date:   Fri Aug 2 20:27:52 2024 +0530
    
        sweet: Move dax-default to device tree
    
        Change-Id: Id2054bb5e61bcfe58706e32f44d37fe7524aa912
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I2d466238525d074269377a82627a816527aefd74
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: basamaryan <basam.aryan@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,9 +66,11 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+
 
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 2
--- common.mk
+++ common.mk
@@ -57,6 +57,7 @@ PRODUCT_PACKAGES += \
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8250/audio
 
+# Audio configs
 PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/kona/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(AUDIO_HAL_DIR)/configs/kona/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
@@ -64,6 +65,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration_a2dp_offload_disabled.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration_a2dp_offload_disabled.xml \
+        $(LOCAL_PATH)/audio/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
     $(LOCAL_PATH)/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_hearing_aid_audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
