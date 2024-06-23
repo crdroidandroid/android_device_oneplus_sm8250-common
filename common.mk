a34c4c16e3bbbfaae0a507f6f19daa59287ad65f
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    sm8250-common: Convert WiFi firmware symlinks to install_symlink targets
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- common.mk
+++ common.mk
@@ -506,6 +506,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# WiFi firmware symlinks
+PRODUCT_PACKAGES += \
+    firmware_wlan_mac.bin_symlink \
+    firmware_WCNSS_qcom_cfg.ini_symlink
+
 # Inherit from the OnePlus Camera makefile.
 $(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
 
