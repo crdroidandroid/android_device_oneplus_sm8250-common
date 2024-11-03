60c285fa41174e78d56a312f2bdc72b94dcc8baf
bojantanasic <bojan.tanasic@gmail.com>
2024-11-03 11:39:26 +0100

    sm8250-common: PRODUCT_DEXPREOPT_SPEED_APPS List of apps that have been identified as core to the products and that are desirable to compile speed compile filter.App are SystemUI and Settings

--- common.mk
+++ common.mk
@@ -398,5 +398,10 @@ PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
+# Speed compiler filter    
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI
+
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8250-common/sm8250-common-vendor.mk)
