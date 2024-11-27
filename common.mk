b85e8b04e2810d7e28bb49e717ee881e07a96548
ShevT <evgeny.shishkov@gmail.com>
2024-11-27 13:35:31 +0300

    sm8250-common: Build XiaomiDolby

--- common.mk
+++ common.mk
@@ -131,6 +131,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
+# Dolby
+PRODUCT_PACKAGES += \
+    XiaomiDolby
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
