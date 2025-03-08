8a1405ceb4751053f86a58cf390d14179fdccc6b
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    sm8250-common: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- extract-files.py
+++ extract-files.py
@@ -11,9 +11,7 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
-    lib_fixup_remove_arch_suffix,
-    lib_fixup_vendorcompat,
+    lib_fixups,
     lib_fixups_user_type,
     libs_clang_rt_ubsan,
     libs_proto_3_9_1,
@@ -55,9 +53,6 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.wifidisplaysession@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
-    (
-        'libwpa_client',
-    ): lib_fixup_remove,
 }
 
 
