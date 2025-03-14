8f0585c7a06b449abda255c7de5fc3ff33057ba7
neobuddy89 <neobuddy89@gmail.com>
2025-03-14 14:19:27 +0000

    sm8250-common: Nuke OnePlus doze

--- common.mk
+++ common.mk
@@ -131,9 +131,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
 
 # Dolby
 PRODUCT_PACKAGES += \
