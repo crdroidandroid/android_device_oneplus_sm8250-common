f5cbb8305e822dd56fa2d9c16eec71a1aa97a7f1
bojantanasic <bojan.tanasic@gmail.com>
2025-04-01 20:42:23 +0200

    sm8250-common: fix compiler error libstagefright_foundation-v33 for Dolby

--- common.mk
+++ common.mk
@@ -372,9 +372,9 @@ PRODUCT_COPY_FILES += \
 # VNDK
 PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+# Dolby
+PRODUCT_PACKAGES += \
+    libstagefright_foundation-v33
 
     
 # WiFi
