fbcb2a5db34d8210f5be72c3da20471e04a40025
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:00:08 +0100

    sm8250-common: Set UDFPS extension lib via soong config
    Change-Id: I8a709bde4843b61420ea1003a749952d2619e2da

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,9 +65,6 @@ TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
-# Fingerprint
-TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
-
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
