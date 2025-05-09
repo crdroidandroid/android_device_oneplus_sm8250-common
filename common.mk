538d1fc764a24b0fc2bbfaf4252f3d4fcd984e9b
bojantanasic <bojan.tanasic@gmail.com>
2025-05-09 21:57:23 +0200

    sm8250-common: Use speed tuning for performance critical applications The default ART setting is quicken: run DEX code verification and optimize some DEX instructions to get better interpreter performance.
    This commit switches performance critical applications to speed:
    run DEX code verification and AOT-compile all methods.
    
    PRODUCT_DEXPREOPT_SPEED_APPS (New in Android O)
    List of applications that have been identified as core to the products
    and which are desirable to compile with the speed compiler filter.  For
    example, persistent apps such as SystemUI get a chance to use
    profile-guided compilation only at the next reboot, so it may be better
    for the product to have these apps always AOT-compiled.
    
    Co-authored-by: xboxfanj <jake@aospa.co>

--- common.mk
+++ common.mk
@@ -374,12 +374,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_USE_PRODUCT_VNDK_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
-     libstagefright_foundation-v33
-
-# PRODUCT_COPY_FILES += \
-#     prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
-#     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+    libstagefright_foundation-v33
     
+# Preopt SystemUI    
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    Settings \
+    SystemUI
+        
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
