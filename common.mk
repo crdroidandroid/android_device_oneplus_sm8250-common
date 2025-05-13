ad6ad15dd99fc1f92026ebe9b7fb803020a6a16f
Shobhit Singh <iam@shobhit.win>
2025-05-13 14:12:11 +0200

    sm8250-common: Set camera override format from reserved via soong config

--- common.mk
+++ common.mk
@@ -109,6 +109,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+
+
+$(call soong_config_set,camera,override_format_from_reserved,true)
+
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
