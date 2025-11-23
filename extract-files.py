145925fa07931be91903fe6f21460eb3ec39cf72
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:24:05 +0100

    sm8250-common: Shim WFD with libinput_shim
    Change-Id: I5c64d4fb6608cf6134ee7647802d9befc26fc8fb

--- extract-files.py
+++ extract-files.py
@@ -103,6 +103,8 @@ blob_fixups: blob_fixups_user_type = {
         .apktool_patch('blob-patches/PowerOffAlarm.patch'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
         .regex_replace('/my_product', '/product'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
