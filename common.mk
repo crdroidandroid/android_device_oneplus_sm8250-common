520e6eb37d7e67c1ce59b101066ec98d6e6bdbfb
SENSEIIIII <mistakesensei@gmail.com>
2025-12-31 17:07:46 +0000

    sm8250-common: Update camera soong config variables to bool type

--- common.mk
+++ common.mk
@@ -264,7 +264,7 @@ PRODUCT_PACKAGES += \
 
 # Inherit from the OnePlus Camera makefile.
 $(call inherit-product, vendor/oneplus/camera/camera-vendor.mk)
-$(call soong_config_set,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
+$(call soong_config_set_bool,camera,override_format_from_reserved,$(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED))
 $(call soong_config_set,camera,package_name,com.oplus.packageName)
 
 # Partitions
