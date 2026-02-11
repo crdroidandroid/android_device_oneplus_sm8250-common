2a378e30aebb892c3424399be7ebbfa0587f543e
bojantanasic <bojan.tanasic@gmail.com>
2026-02-11 10:06:22 +0100

    sm8250-common: Add Dolby to device framework matrix

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -65,6 +65,7 @@ TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/device_framework_matrix.xml \
     hardware/oplus/vintf/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
